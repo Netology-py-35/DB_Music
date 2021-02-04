767829a779bac80f918dfe0a36c777f52de03c5d
Oleg Kishinskiy <oleg@kishinskiy.com>
2021-02-04 18:00:03 +0300

    add selects

--- Select.sql
+++ Select.sql
@@ -1,5 +1,11 @@
 SELECT name, year FROM Albums WHERE year = 2018;
 
--- SELECT name, time FROM Tracks WHERE
+SELECT name, time FROM Tracks WHERE MAX(time);
 
-SELECT name FROM Tracks WHERE time <= '3:30'
+SELECT name FROM Tracks WHERE time <= '3:30';
+
+SELECT name FROM Collections WHERE year >= 2018 AND year <= 2020;
+
+SELECT name FROM Artists WHERE name NOT LIKE '% %';
+
+SELECT name FROM Tracks WHERE name LIKE '%% my %%';
